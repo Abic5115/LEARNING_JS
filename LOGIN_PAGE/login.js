666b73dff99360fcd08ce399893b99985bb2d069
Abic5115 <139050132+Abic5115@users.noreply.github.com>
2024-10-10 10:42:38 +0530

    Add files via upload

--- LOGIN_PAGE/login.js
+++ LOGIN_PAGE/login.js
@@ -1,7 +1,29 @@
-let button1 = document.getElementById("button")
+var button1 = document.getElementById("button")
 
-button1.addEventListener('click',()=>{
-    let usname = document.getElementById("username").value
-    // let pass = document.getElementById("password").value
-    console.log(usname)
+button1.addEventListener('click', () => {
+    let user1 = document.getElementById("username").value
+    let pass1 = document.getElementById("password").value
+ 
+    let storeddetails = JSON.parse(localStorage.getItem('studentdet') || '[]')
+    let connect = false;
+    for(let i=0;i<storeddetails.length;i++)
+      {
+        if ( storeddetails[i].username1  === user1 &&  storeddetails[i].password1===  pass1 ){
+            connect =true;
+        break;}
+      }
+
+
+     if(connect )
+     {
+        window.location.href = "welcome.html";
+
+
+    }
+    else {
+        document.getElementById("message").innerHTML = "invalid user ..please sign up"
+    }
+
 })
+
+
--- LOGIN_PAGE/signup.js
+++ LOGIN_PAGE/signup.js
@@ -5,40 +5,82 @@ let messagepop3 = document.getElementById("message3")
 
 button1.addEventListener('click', () => {
 
+   
+  
+     
     let user = document.getElementById('username').value
-    if (user.length < 8) {
-        messagepop1.textContent = "length is less than 8"
-    }
+    let userflag =false
+    if (user.length < 6) {
+        messagepop1.textContent = "length is less than 6"
+       
+    } 
     else {
         console.log("username:", user)
-        localStorage.setItem('username',JSON.stringify(user))
+        userflag = true
     }
+
+
     let mail = document.getElementById('email').value
+    let mailflag=false
     if ((!mail.endsWith('@gmail.com')) && (!mail.endsWith('@yahoo.com'))) {
         messagepop2.textContent = "enter a valid email id"
+        
     }
+    
     else {
         console.log("mail:", mail)
-        localStorage.setItem('gmail',JSON.stringify(mail))
+        mailflag=true
+       
     }
+    let passflag=false
     const specialCharPattern = /[!@#$%^&*(),.?":{}|<>]/;
     const number = /[0123456789]/;
     let pass = document.getElementById('password').value
     if (pass.length < 6) {
         messagepop3.textContent = "enter password with minimum 6 characters"
+       
     }
     else if (!specialCharPattern.test(pass)) {
         messagepop3.textContent = "enter password with one specialcharacter"
+        
     }
     else if(!number.test(pass)){
         messagepop3.textContent = "enter password with number"
+        
     }
-
+    
+ 
 else {
         console.log("password:", pass)
-        localStorage.setItem('password',JSON.stringify(pass))
+       passflag=true
     }
+    if( userflag==true && mailflag ==true && passflag==true){
+   let check = JSON.parse(localStorage.getItem ('studentdet')) || []
+    let details=[]
+    
+        let signupdetails ={
+            username1:user,
+            email1:mail,
+            password1:pass
+        };
+         
+        check.push(signupdetails)
+        localStorage.setItem('studentdet', JSON.stringify(check))
+        document.getElementById('username').value = '';
+        document.getElementById('email').value = '';
+        document.getElementById('password').value = '';
+    }
+else{
+    alert("enter valid details")
+}
 
-    console.log("email:", mail)
-    console.log("password:", pass)
+
+
+
+
+
+
+
+
+
 })
